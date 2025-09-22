1811a22c0478476b34fe21537c503c4ed4f7d7ee
Thaisrs165 <thaisrs3@al.insper.edu.br>
2025-09-22 10:31:03 -0300

    .

--- test_routes.py
+++ test_routes.py
@@ -7,27 +7,27 @@ from datetime import date
 @pytest.fixture
 def filme():
     app.config["TESTING"] = True
-    return app.test_filme() 
+    return app.test_client() 
 
 
 @patch("app.criar_objeto_filme")
-def test_criar_filme_route(mock_criar, filme):
+def test_criar_filme_route(mock_criar, client):
     mock_criar.return_value = {"id": "123", "titulo": "Jurassic", "descricao": "filme de dinossauro", "duracao": 2, "diretor": "Spilberg", "dataCadastro": date.today()}
 
-    response = filme.post("/filmes", json={"titulo": "Jurassic", "descricao": "filme de dinossauro", "duracao": 2, "diretor": "Spilberg", "dataCadastro": date.today()})
+    response = client.post("/filmes", json={"titulo": "Jurassic", "descricao": "filme de dinossauro", "duracao": 2, "diretor": "Spilberg", "dataCadastro": date.today()})
     data = response.get_json()
 
     assert response.status_code == 201
     assert data["id"] == "123"
 
-def test_criar_filme_route_missing_field(filme):
-    response = filme.post("/filmes", json={"titulo": "Jurassic", "descricao": "filme de dinossauro", "duracao": 2, "diretor": "Spilberg", "dataCadastro": date.today()})
+def test_criar_filme_route_missing_field(client):
+    response = client.post("/filmes", json={"titulo": "Jurassic", "descricao": "filme de dinossauro", "duracao": 2, "diretor": "Spilberg", "dataCadastro": date.today()})
     data = response.get_json()
     assert response.status_code == 400
     assert "titulo" in data["erro"]
 
 @patch("app_service.listar_filmes") 
-def test_listar_filmes_route(mock_listar, filme):
+def test_listar_filmes_route(mock_listar, client):
     mock_listar.return_value = [{"id": "123", "titulo": "Jurassic", "descricao": "filme de dinossauro", "duracao": 2, "diretor": "Spilberg", "dataCadastro": date.today()}]
 
     response = filme.get("/filmes")
@@ -38,17 +38,17 @@ def test_listar_filmes_route(mock_listar, filme):
 
 
 @patch("app.deletar_filme") 
-def test_deletar_filme_route(mock_deletar, filme):
+def test_deletar_filme_route(mock_deletar, client):
     mock_deletar.return_value = True
 
-    response = filme.delete("/filmes/123")
+    response = client.delete("/filmes/123")
     data = response.get_json()
 
     assert response.status_code == 200
     assert "sucesso" in data["mensagem"]
 
 @patch("app_service.deletar_filme")
-def test_deletar_filme_route_not_found(mock_deletar, filme):
+def test_deletar_filme_route_not_found(mock_deletar, client):
     mock_deletar.return_value = False
-    response = filme.delete("/filmes/999")
+    response = client.delete("/filmes/999")
     assert response.status_code == 404
